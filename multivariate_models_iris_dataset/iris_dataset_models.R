0b866bad43f049bf386431b955258b29c654749a
Toks <mthiamang@gmail.com>
2016-02-04 17:05:49 -0500

    building multivariate statistical models in R with Iris dataset

--- multivariate_models_iris_dataset/iris_dataset_models.R
+++ multivariate_models_iris_dataset/iris_dataset_models.R
@@ -1,5 +1,12 @@
 #install.packages("caret")
+#install.packages("ellipse")
+#install.packages("e1071")
+#install.packages("rpart")
+install.packages("kernlab")
+library (rpart)
 library(caret)
+library(ellipse)
+library(e1071)
 data("iris")
 dataset <- iris
 head(dataset)
@@ -30,5 +37,35 @@ for (i in 1:4){
 plot(y)
 #Multivariate Plots
 par(mfrow=c(1,1))
-featurePlot(x, y, "ellipse")
 featurePlot(x=x, y=y, plot="ellipse")
+featurePlot(x=x, y=y, plot="box")
+scales <- list(x=list(relation="free"),y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+#Test Harness
+#Cross Validation
+control <- trainControl(method = "cv", number=10)
+metric <- "Accuracy"
+
+#Linear Models - Let us draw a stright line through everything!
+set.seed(7)
+fit.lda <-train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+#Non Linear
+#CART
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+#kNN
+fit.knn<- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+#SVM
+fit.svm <-train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+#Random Forest
+fit.rf<- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+
+#Accuracy of Results
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
+dotplot(results)
+print(fit.lda)
+
+#Predictions
+predictions<-predict(fit.lda, validation)
+confusionMatrix(predictions, validation$Species)
